c31044d5cae422cac1a0ed5049c67267029f5849
Nada-Karim <105967335+Nada-Karim@users.noreply.github.com>
2023-01-04 02:34:13 +0200

    Update shop.php

--- shop.php
+++ shop.php
@@ -14,7 +14,7 @@
    <link rel="stylesheet" href="css/bootstrap.min.css" />
    <link rel="stylesheet" href="css/all.min.css" />
    <link rel="stylesheet" href="css/Web.css" />
-   <link rel="stylesheet" href="css/Wishlist.css">
+
    <link rel="stylesheet" href="css/Nav.css">
    <link rel="stylesheet" href="css/CartButton.css">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
@@ -79,283 +79,21 @@ outline: none;
  
 }
 
-
+.heart-icon1 {
+    font-size: 20px;
+    color:hsl(113, 74%, 9%);
+}
+.heart-icon1:hover{
+	
+	color:#ff0000;
+}
+.heart-icon1:focus{
+	
+	color:#ff0000;
+}
 
 
 /*start of nav bar style*/
-
-.dropdown:hover .dropdown-menu {
-        display: block;
-        margin-top: 0; 
-     }
-    
-     .static {
-  position:absolute;
-  background: white;
-}
-
-.static:hover {
-  opacity:0;
-}
-    h6#d1{
-    visibility:hidden ;
-    padding:1px;
-    margin-top: 9px; 
-    color:#C4AE78;
-    }
-    
-
-  
-
-
-   
-    .btn-default{
-    border-width:2px;
-
-
-    }
-    
-    
-    .form-outline{
-    border-width:2px;
-    }
-    
-    input[type=search] {  
-    
-    margin-top: 13px;  
-    font-size: 14px;  
-    width :170px;
-  margin-right: 2px; 
-
-   }  
-    
-
-
-
-    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900');
-
-
-a {
-	cursor: pointer;
-  transition: all 200ms linear;
-}
-a:hover {
-	text-decoration: none;
-}
-.link {
-  color: #C4AE78;
-}
-.link:hover {
-  color: #C4AE78;
-}
-p {
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 1.7;
-}
-h4 {
-  font-weight: 600;
-}
-h6 span{
-  padding: 0 20px;
-  text-transform: uppercase;
-  font-weight: 700;
-}
-.section{
-  position: relative;
-  width: 100%;
-  display: block;
-}
-.full-height{
-  min-height: 100vh;
-}
-[type="checkbox"]:checked,
-[type="checkbox"]:not(:checked){
-  position: absolute;
-  left: -9999px;
-}
-.checkbox:checked + label,
-.checkbox:not(:checked) + label{
-  position: relative;
-  display: block;
-  text-align: center;
-  width: 60px;
-  height: 16px;
-  border-radius: 8px;
-  padding: 0;
-  margin: 10px auto;
-  cursor: pointer;
-  background-color: hsl(113, 74%, 9%);;
-}
-.checkbox:checked + label:before,
-.checkbox:not(:checked) + label:before{
-  position: absolute;
-  display: block;
-  width: 36px;
-  height: 36px;
-  border-radius: 50%;
-  color: #fff;
-  background-color: hsl(113, 74%, 9%);;
-  font-family: 'unicons';
-  content: '\eb4f';
-  z-index: 20;
-  top: -10px;
-  left: -10px;
-  line-height: 36px;
-  text-align: center;
-  font-size: 24px;
-  transition: all 0.5s ease;
-}
-.checkbox:checked + label:before {
-  transform: translateX(44px) rotate(-270deg);
-}
-
-
-.card-3d-wrap {
-  position: relative;
-  width: 300px;
-  max-width: 100%;
-  height: 250px;
-  -webkit-transform-style: preserve-3d;
-  transform-style: preserve-3d;
-  perspective: 800px;
-  margin-top: 40px;
-}
-.card-3d-wrapper {
-  width: 100%;
-  height: 100%;
-  position:absolute;    
-  top: 0;
-  left: 0;  
-  -webkit-transform-style: preserve-3d;
-  transform-style: preserve-3d;
-  transition: all 600ms ease-out; 
-}
-.card-front, .card-back {
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-  
-  background-position: bottom center;
-  background-repeat: no-repeat;
-  background-size: 300%;
-  position: absolute;
-  border-radius: 6px;
-  left: 0;
-  top: 0;
-  -webkit-transform-style: preserve-3d;
-  transform-style: preserve-3d;
-  -webkit-backface-visibility: hidden;
-  -moz-backface-visibility: hidden;
-  -o-backface-visibility: hidden;
-  backface-visibility: hidden;
-}
-.card-back {
-  transform: rotateY(180deg);
-}
-.checkbox:checked ~ .card-3d-wrap .card-3d-wrapper {
-  transform: rotateY(180deg);
-}
-.center-wrap{
-  position: absolute;
-  width: 100%;
-  padding: 0 23px;
-  top: 50%;
-  left: 0;
-  transform: translate3d(0, -50%, 35px) perspective(100px);
-  z-index: 20;
-  display: block;
-}
-
-
-.form-group{ 
-  position: relative;
-  display: block;
-    margin: 0;
-    padding: 0;
-}
-.form-style {
-  padding: 13px 20px;
-  padding-left: 50px;
-  height: 40px;
-  width: 100%;
-  font-weight: 500;
-  border-radius: 4px;
-  font-size: 14px;
-  line-height: 20px;
-  letter-spacing: 0.5px;
-  outline: none;
-  color: hsl(113, 74%, 9%);
-  background-color: hsl(113, 74%, 9%);
-  border: none;
-  -webkit-transition: all 200ms linear;
-  transition: all 200ms linear;
-  box-shadow: 0 4px 8px 0 rgba(21,21,21,.2);
-}
-.form-style:focus,
-.form-style:active {
-  border: none;
-  outline: none;
-  box-shadow: 0 4px 8px 0 rgba(21,21,21,.2);
-}
-.input-icon {
-  position: absolute;
-  top: 0;
-  left: 18px;
-  height: 40px;
-  font-size: 14px;
-  line-height: 0px;
-  text-align: left;
-  color: #ffeba7;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-
-.form-group input:-ms-input-placeholder  {
-  color: #c4c3ca;
-  opacity: 0.7;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-.form-group input::-moz-placeholder  {
-  color: #c4c3ca;
-  opacity: 0.7;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-.form-group input:-moz-placeholder  {
-  color: #c4c3ca;
-  opacity: 0.7;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-.form-group input::-webkit-input-placeholder  {
-  color: #c4c3ca;
-  opacity: 0.7;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-.form-group input:focus:-ms-input-placeholder  {
-  opacity: 0;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-.form-group input:focus::-moz-placeholder  {
-  opacity: 0;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-.form-group input:focus:-moz-placeholder  {
-  opacity: 0;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-.form-group input:focus::-webkit-input-placeholder  {
-  opacity: 0;
-  -webkit-transition: all 200ms linear;
-    transition: all 200ms linear;
-}
-
 
 
 
@@ -442,7 +180,7 @@ while ($row = $result2->fetch_assoc()) {
         echo '<div class="card-group" style="float: left; margin-top: 2cm; transle">';
         echo '
           <div class="card" style="width: 18rem; height:37rem;">
-            <div class="container" style="width: 18rem; height:37rem;">
+            <div class="container" style="width: 18rem; height:42rem;">
               <div class="flip-box">
                 <div class="flip-box-inner">
                   <div class="flip-box-front">
@@ -505,8 +243,9 @@ while ($row = $result2->fetch_assoc()) {
       <p class="card-text">';
         echo "<p>EGP " . $row["BookPrice"];
         echo'
-         <button class="fas fa-heart" type="submit" name="wishlist" style="background-color: transparent;" onclick="trial('.$row["BookID"].')"></button>';
       
+    <a href="" class="heart-icon1 el" onclick="trial('.$row["BookID"].')"><i
+        class="fa-solid fa-heart"></i></a> ';
         echo' </p>
       </p></div></div> </div>';
       }
